534e39d8f7956b8f5cdea87b6fc511989b66e6d9
htt <641571835@qq.com>
2022-02-26 19:57:52 +0800

    upgrade ksp version: 1.6.10-1.0.2 -> 1.6.10-1.0.4

--- ktorm-ksp-compiler/build.gradle.kts
+++ ktorm-ksp-compiler/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 dependencies {
     api(project(":ktorm-ksp-api"))
     api("org.ktorm:ktorm-core:3.4.1")
-    api("com.google.devtools.ksp:symbol-processing-api:1.6.10-1.0.2")
+    api("com.google.devtools.ksp:symbol-processing-api:1.6.10-1.0.4")
     api("com.squareup:kotlinpoet-ksp:1.10.2")
 }
