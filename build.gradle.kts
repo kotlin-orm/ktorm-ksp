dee14d85ab7ac0ad6d2ea0475ec5908139c9a418
htt <641571835@qq.com>
2022-02-27 11:55:07 +0800

    upgrade ksp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.10" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.6.10"))
+    }
 }
 
 allprojects {
