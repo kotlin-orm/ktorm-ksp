fab4bb7c0148c3a179a90449e613266a2f62ef92
夜里的向日葵 <641571835@qq.com>
2022-05-13 09:34:21 +0800

    add detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,25 @@ allprojects {
         options.encoding = "UTF-8"
     }
 
+    val detekt by configurations.creating
+    val detektVersion:String by project
+
+    tasks.register<JavaExec>("detekt") {
+        mainClass.set("io.gitlab.arturbosch.detekt.cli.Main")
+        classpath = detekt
+        val input = "${projectDir}/src/main/kotlin"
+        val config = "${rootDir}/detekt.yml"
+        val params = listOf("-i", input, "-c", config)
+        args(params)
+    }
+
+    dependencies {
+        detekt("io.gitlab.arturbosch.detekt:detekt-cli:${detektVersion}")
+        detekt("io.gitlab.arturbosch.detekt:detekt-formatting:${detektVersion}")
+    }
+
     repositories {
         mavenCentral()
         jcenter()
     }
-
 }
