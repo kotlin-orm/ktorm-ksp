463a30f832ed6f9ab490c2a453325500decfc189
lookup-cat <641571835@qq.com>
2022-06-26 15:52:44 +0800

    add more logs

--- ktorm-ksp-compiler/src/main/kotlin/org/ktorm/ksp/compiler/KtormProcessorProvider.kt
+++ ktorm-ksp-compiler/src/main/kotlin/org/ktorm/ksp/compiler/KtormProcessorProvider.kt
@@ -130,64 +130,73 @@ public class KtormProcessor(
     ) : KSVisitorVoid() {
 
         override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
-            val kspConfig = classDeclaration.getAnnotationsByType(KtormKspConfig::class).first()
-            val kspConfigAnnotation = classDeclaration.annotations.first {
-                it.annotationType.resolve().toClassName() == KtormKspConfig::class.asClassName()
-            }
-            val argumentMap = kspConfigAnnotation.arguments.associateBy { it.name!!.asString() }
-            configBuilder.allowReflectionCreateEntity = kspConfig.allowReflectionCreateClassEntity
-            configBuilder.extension = ExtensionGeneratorConfig(
-                kspConfig.extension.enableSequenceOf,
-                kspConfig.extension.enableClassEntitySequenceAddFun,
-                kspConfig.extension.enableClassEntitySequenceUpdateFun,
-            )
-            //namingStrategy
-            val namingStrategyType = argumentMap[KtormKspConfig::namingStrategy.name]!!.value as KSType
-            if (namingStrategyType.toClassName() != Nothing::class.asClassName()) {
-                if ((namingStrategyType.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
-                    error("Wrong KtormKspConfig parameter:${KtormKspConfig::namingStrategy.name}, namingStrategy must be singleton.")
+            logger.info("KtormKspConfigVisitor visitClassDeclaration: ${classDeclaration.toClassName()}")
+            try {
+                val kspConfig = classDeclaration.getAnnotationsByType(KtormKspConfig::class).first()
+                val kspConfigAnnotation = classDeclaration.annotations.first {
+                    it.annotationType.resolve().toClassName() == KtormKspConfig::class.asClassName()
                 }
-                configBuilder.namingStrategy = namingStrategyType.toClassName()
-                try {
-                    @Suppress("KotlinConstantConditions")
-                    configBuilder.localNamingStrategy =
-                        Class.forName(namingStrategyType.declaration.qualifiedName!!.asString()).kotlin.objectInstance as NamingStrategy
-                } catch (e: Exception) {
-                    // ignore
-                }
-            }
-
-            // enum converter
-            val enumConverterType = argumentMap[KtormKspConfig::enumConverter.name]!!.value as KSType
-            if (enumConverterType.toClassName() != Nothing::class.asClassName()) {
-                if ((enumConverterType.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
-                    error("Wrong KtormKspConfig parameter:${KtormKspConfig::enumConverter.name}, converter must be singleton.")
-                }
-                configBuilder.enumConverter = ConverterDefinition(
-                    enumConverterType.toClassName(), enumConverterType.declaration as KSClassDeclaration
+                val argumentMap = kspConfigAnnotation.arguments.associateBy { it.name!!.asString() }
+                configBuilder.allowReflectionCreateEntity = kspConfig.allowReflectionCreateClassEntity
+                configBuilder.extension = ExtensionGeneratorConfig(
+                    kspConfig.extension.enableSequenceOf,
+                    kspConfig.extension.enableClassEntitySequenceAddFun,
+                    kspConfig.extension.enableClassEntitySequenceUpdateFun,
                 )
-            }
-
-            // single type converter
-            @Suppress("UNCHECKED_CAST") val singleTypeConverters =
-                argumentMap[KtormKspConfig::singleTypeConverters.name]!!.value as List<KSType>
-            if (singleTypeConverters.isNotEmpty()) {
-                val singleTypeConverterMap = singleTypeConverters.asSequence()
-                    .onEach {
-                        if ((it.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
-                            error("Wrong KtormKspConfig parameter:${KtormKspConfig::singleTypeConverters.name} value:${it.declaration.qualifiedName!!.asString()} converter must be singleton.")
-                        }
-                    }.associate {
-                        val singleTypeReference =
-                            (it.declaration as KSClassDeclaration).findSuperTypeReference(SingleTypeConverter::class.qualifiedName!!)!!
-                        val supportType = singleTypeReference.resolve().arguments.first().type!!.resolve().toClassName()
-                        val converterDefinition =
-                            ConverterDefinition(it.toClassName(), it.declaration as KSClassDeclaration)
-                        supportType to converterDefinition
+                //namingStrategy
+                val namingStrategyType = argumentMap[KtormKspConfig::namingStrategy.name]!!.value as KSType
+                if (namingStrategyType.toClassName() != Nothing::class.asClassName()) {
+                    if ((namingStrategyType.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
+                        error("Wrong KtormKspConfig parameter:${KtormKspConfig::namingStrategy.name}, namingStrategy must be singleton.")
                     }
-                configBuilder.singleTypeConverters = singleTypeConverterMap
-            }
+                    configBuilder.namingStrategy = namingStrategyType.toClassName()
+                    try {
+                        @Suppress("KotlinConstantConditions")
+                        configBuilder.localNamingStrategy =
+                            Class.forName(namingStrategyType.declaration.qualifiedName!!.asString()).kotlin.objectInstance as NamingStrategy
+                    } catch (e: Exception) {
+                        // ignore
+                    }
+                }
 
+                // enum converter
+                val enumConverterType = argumentMap[KtormKspConfig::enumConverter.name]!!.value as KSType
+                if (enumConverterType.toClassName() != Nothing::class.asClassName()) {
+                    if ((enumConverterType.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
+                        error("Wrong KtormKspConfig parameter:${KtormKspConfig::enumConverter.name}, converter must be singleton.")
+                    }
+                    configBuilder.enumConverter = ConverterDefinition(
+                        enumConverterType.toClassName(), enumConverterType.declaration as KSClassDeclaration
+                    )
+                }
+
+                // single type converter
+                @Suppress("UNCHECKED_CAST") val singleTypeConverters =
+                    argumentMap[KtormKspConfig::singleTypeConverters.name]!!.value as List<KSType>
+                if (singleTypeConverters.isNotEmpty()) {
+                    val singleTypeConverterMap = singleTypeConverters.asSequence()
+                        .onEach {
+                            if ((it.declaration as KSClassDeclaration).classKind != ClassKind.OBJECT) {
+                                error("Wrong KtormKspConfig parameter:${KtormKspConfig::singleTypeConverters.name} value:${it.declaration.qualifiedName!!.asString()} converter must be singleton.")
+                            }
+                        }.associate {
+                            val singleTypeReference =
+                                (it.declaration as KSClassDeclaration).findSuperTypeReference(SingleTypeConverter::class.qualifiedName!!)!!
+                            val supportType =
+                                singleTypeReference.resolve().arguments.first().type!!.resolve().toClassName()
+                            val converterDefinition =
+                                ConverterDefinition(it.toClassName(), it.declaration as KSClassDeclaration)
+                            supportType to converterDefinition
+                        }
+                    configBuilder.singleTypeConverters = singleTypeConverterMap
+                }
+            } catch (e: Exception) {
+                logger.error(
+                    "KtormKspConfigVisitor visitClassDeclaration error. className:" +
+                            "${classDeclaration.toClassName()} file:${classDeclaration.containingFile?.filePath}"
+                )
+                throw e
+            }
         }
 
     }
@@ -204,87 +213,96 @@ public class KtormProcessor(
             data: Unit,
         ) {
             val entityClassName = classDeclaration.toClassName()
-            val ktormEntityType = when (classDeclaration.classKind) {
-                ClassKind.INTERFACE -> {
-                    val entityQualifiedName = Entity::class.qualifiedName
-                    classDeclaration.findSuperTypeReference(entityQualifiedName!!)
-                        ?: error("wrong entity class declaration: ${entityClassName.canonicalName}, Entity of interface type must inherit [${entityQualifiedName}]")
-                    KtormEntityType.ENTITY_INTERFACE
-                }
-                ClassKind.CLASS -> KtormEntityType.ANY_KIND_CLASS
-                else -> error("wrong entity class declaration: ${entityClassName.canonicalName}, classKind must to be Interface or Class")
-            }
-            val table = classDeclaration.getAnnotationsByType(Table::class).first()
-            val tableClassName = if (table.tableClassName.isEmpty()) {
-                ClassName(entityClassName.packageName, entityClassName.simpleName.pluralNoun())
-            } else {
-                ClassName(entityClassName.packageName, table.tableClassName)
-            }
-            val tableName = table.tableName
-
-            val columnDefs = mutableListOf<ColumnDefinition>()
-            val tableDef = TableDefinition(
-                tableName,
-                tableClassName,
-                table.alias,
-                table.catalog,
-                table.schema,
-                entityClassName,
-                columnDefs,
-                classDeclaration.containingFile!!,
-                classDeclaration,
-                ktormEntityType
-            )
-            tableDefinitions.add(tableDef)
-            // parse column definition
-            classDeclaration.getAllProperties()
-                .forEach { ksProperty ->
-                    val propertyKSType = ksProperty.type.resolve()
-                    val propertyName = ksProperty.simpleName.asString()
-                    if (ksProperty.isAnnotationPresent(Ignore::class) || propertyName in table.ignoreColumns
-                        || (tableDef.ktormEntityType == KtormEntityType.ENTITY_INTERFACE && propertyName in ignoreInterfaceEntityProperties)
-                    ) {
-                        logger.info("ignore column: ${tableDef.entityClassName.canonicalName}.$propertyName")
-                        return@forEach
+            logger.info("EntityVisitor visitClassDeclaration: $entityClassName")
+            try {
+                val ktormEntityType = when (classDeclaration.classKind) {
+                    ClassKind.INTERFACE -> {
+                        val entityQualifiedName = Entity::class.qualifiedName
+                        classDeclaration.findSuperTypeReference(entityQualifiedName!!)
+                            ?: error("wrong entity class declaration: ${entityClassName.canonicalName}, Entity of interface type must inherit [${entityQualifiedName}]")
+                        KtormEntityType.ENTITY_INTERFACE
                     }
-                    val columnAnnotation = ksProperty.getAnnotationsByType(Column::class).firstOrNull()
-                    val ksColumnAnnotation =
-                        ksProperty.annotations.firstOrNull { anno -> anno.annotationType.resolve().declaration.qualifiedName?.asString() == columnQualifiedName }
-                    //converter
-                    val converter =
-                        ksColumnAnnotation?.arguments?.firstOrNull { anno -> anno.name?.asString() == Column::converter.name }?.value as KSType?
-                    var converterDefinition: ConverterDefinition? = null
-                    if (converter != null && converter.toClassName() != Nothing::class.asClassName()) {
-                        val converterDeclaration = converter.declaration as KSClassDeclaration
-                        if (converterDeclaration.classKind != ClassKind.OBJECT) {
-                            error("Wrong converter type:${converter.toClassName()}, converter must be singleton.")
+                    ClassKind.CLASS -> KtormEntityType.ANY_KIND_CLASS
+                    else -> error("wrong entity class declaration: ${entityClassName.canonicalName}, classKind must to be Interface or Class")
+                }
+                val table = classDeclaration.getAnnotationsByType(Table::class).first()
+                val tableClassName = if (table.tableClassName.isEmpty()) {
+                    ClassName(entityClassName.packageName, entityClassName.simpleName.pluralNoun())
+                } else {
+                    ClassName(entityClassName.packageName, table.tableClassName)
+                }
+                val tableName = table.tableName
+
+                val columnDefs = mutableListOf<ColumnDefinition>()
+                val tableDef = TableDefinition(
+                    tableName,
+                    tableClassName,
+                    table.alias,
+                    table.catalog,
+                    table.schema,
+                    entityClassName,
+                    columnDefs,
+                    classDeclaration.containingFile!!,
+                    classDeclaration,
+                    ktormEntityType
+                )
+                tableDefinitions.add(tableDef)
+                // parse column definition
+                classDeclaration.getAllProperties()
+                    .forEach { ksProperty ->
+                        val propertyKSType = ksProperty.type.resolve()
+                        val propertyName = ksProperty.simpleName.asString()
+                        if (ksProperty.isAnnotationPresent(Ignore::class) || propertyName in table.ignoreColumns
+                            || (tableDef.ktormEntityType == KtormEntityType.ENTITY_INTERFACE && propertyName in ignoreInterfaceEntityProperties)
+                        ) {
+                            logger.info("ignore column: ${tableDef.entityClassName.canonicalName}.$propertyName")
+                            return@forEach
                         }
-                        converterDefinition = ConverterDefinition(converter.toClassName(), converterDeclaration)
-                    }
+                        val columnAnnotation = ksProperty.getAnnotationsByType(Column::class).firstOrNull()
+                        val ksColumnAnnotation =
+                            ksProperty.annotations.firstOrNull { anno -> anno.annotationType.resolve().declaration.qualifiedName?.asString() == columnQualifiedName }
+                        //converter
+                        val converter =
+                            ksColumnAnnotation?.arguments?.firstOrNull { anno -> anno.name?.asString() == Column::converter.name }?.value as KSType?
+                        var converterDefinition: ConverterDefinition? = null
+                        if (converter != null && converter.toClassName() != Nothing::class.asClassName()) {
+                            val converterDeclaration = converter.declaration as KSClassDeclaration
+                            if (converterDeclaration.classKind != ClassKind.OBJECT) {
+                                error("Wrong converter type:${converter.toClassName()}, converter must be singleton.")
+                            }
+                            converterDefinition = ConverterDefinition(converter.toClassName(), converterDeclaration)
+                        }
 
-                    val isPrimaryKey = ksProperty.getAnnotationsByType(PrimaryKey::class).any()
-                    val columnName = columnAnnotation?.columnName ?: ""
-                    val tablePropertyName = if (columnAnnotation?.propertyName.isNullOrEmpty()) {
-                        MemberName(tableClassName, propertyName)
-                    } else {
-                        MemberName(tableClassName, columnAnnotation!!.propertyName)
+                        val isPrimaryKey = ksProperty.getAnnotationsByType(PrimaryKey::class).any()
+                        val columnName = columnAnnotation?.columnName ?: ""
+                        val tablePropertyName = if (columnAnnotation?.propertyName.isNullOrEmpty()) {
+                            MemberName(tableClassName, propertyName)
+                        } else {
+                            MemberName(tableClassName, columnAnnotation!!.propertyName)
+                        }
+                        val columnDef = ColumnDefinition(
+                            columnName,
+                            isPrimaryKey,
+                            propertyKSType.toClassName(),
+                            MemberName(entityClassName, propertyName),
+                            tablePropertyName,
+                            converterDefinition,
+                            ksProperty,
+                            propertyKSType,
+                            tableDef,
+                            columnAnnotation?.isReferences ?: false,
+                            null
+                        )
+                        columnDefs.add(columnDef)
                     }
-                    val columnDef = ColumnDefinition(
-                        columnName,
-                        isPrimaryKey,
-                        propertyKSType.toClassName(),
-                        MemberName(entityClassName, propertyName),
-                        tablePropertyName,
-                        converterDefinition,
-                        ksProperty,
-                        propertyKSType,
-                        tableDef,
-                        columnAnnotation?.isReferences ?: false,
-                        null
-                    )
-                    columnDefs.add(columnDef)
-                }
-            entityTableMap[entityClassName] = tableClassName
+                entityTableMap[entityClassName] = tableClassName
+            } catch (e: Exception) {
+                logger.error(
+                    "EntityVisitor visitClassDeclaration error. className:" +
+                            "${classDeclaration.toClassName()} file:${classDeclaration.containingFile?.filePath}"
+                )
+                throw e
+            }
         }
 
 
