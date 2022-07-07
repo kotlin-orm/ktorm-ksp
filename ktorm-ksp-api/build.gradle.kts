6f661f75609f0b29a540dc7867830a21ca5443a3
lookup-cat <641571835@qq.com>
2022-07-07 09:24:28 +0800

    add EntityUtilTest.kt

--- ktorm-ksp-api/build.gradle.kts
+++ ktorm-ksp-api/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     api(kotlin("stdlib"))
     api(libs.ktorm.core)
     api(libs.cglib)
+    testImplementation(libs.junit)
+    testImplementation(libs.assertj.core)
 }
 
 configureMavenPublishing()
