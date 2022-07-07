c5af3f3af8b8322901bedde1e169483092483ce4
lookup-cat <641571835@qq.com>
2022-07-07 09:27:43 +0800

    refactoring EntityUtil

--- ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/EntityUtil.kt
+++ ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/EntityUtil.kt
@@ -59,56 +59,71 @@ public object EntityUtil {
     public inline fun <reified T> undefined(): T {
         return undefinedValues.computeIfAbsent(T::class.java) { cls: Class<*> ->
             if (cls.isInterface) {
-                val handler = object : InvocationHandler {
-                    override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any {
-                        return when (method.name) {
-                            "hashCode" -> this.hashCode()
-                            "equals" -> proxy === args!!.first()
-                            "toString" -> this.toString()
-                            else -> throw NotImplementedError()
-                        }
-                    }
-                }
-                try {
-                    Proxy.newProxyInstance(cls.classLoader, arrayOf(cls), handler)
-                } catch (e: Exception) {
-                    throw CreateUndefinedException("Failed to create instance with jdk dynamic proxy", e)
-                }
+                createJdkDynamicProxy(cls)
             } else if (Modifier.isAbstract(cls.modifiers)) {
-                try {
-                    val enhancer = Enhancer()
-                    enhancer.setSuperclass(cls)
-                    enhancer.classLoader = cls.classLoader
-                    enhancer.setCallback(object : MethodInterceptor {
-                        override fun intercept(
-                            obj: Any?,
-                            method: Method,
-                            args: Array<out Any>?,
-                            proxy: MethodProxy
-                        ): Any? {
-                            if (method.name == "hashCode"
-                                || method.name == "equals"
-                                || method.name == "toString"
-                            ) {
-                                return proxy.invokeSuper(obj, args)
-                            }
-                            throw NotImplementedError()
-                        }
-                    })
-                    enhancer.create()
-                } catch (e: Exception) {
-                    throw CreateUndefinedException("Failed to create instance with cglib", e)
-                }
+                createCglibProxy(cls)
             } else {
-                try {
-                    val field = Unsafe::class.java.getDeclaredField("theUnsafe")
-                    field.isAccessible = true
-                    (field.get(null) as Unsafe).allocateInstance(cls)
-                } catch (e: Exception) {
-                    throw CreateUndefinedException("Failed to create instance with Unsafe", e)
-                }
+                createUnsafeInstance(cls)
             }
         } as T
+    }
+
+    @PublishedApi
+    internal fun createJdkDynamicProxy(cls: Class<*>): Any {
+        val handler = object : InvocationHandler {
+            override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any {
+                return when (method.name) {
+                    "hashCode" -> this.hashCode()
+                    "equals" -> proxy === args!!.first()
+                    "toString" -> this.toString()
+                    else -> throw NotImplementedError()
+                }
+            }
+        }
+        try {
+            return Proxy.newProxyInstance(cls.classLoader, arrayOf(cls), handler)
+        } catch (e: Exception) {
+            throw CreateUndefinedException("Failed to create instance with jdk dynamic proxy", e)
+        }
+    }
+
+    @PublishedApi
+    internal fun createCglibProxy(cls: Class<*>): Any {
+        try {
+            val enhancer = Enhancer()
+            enhancer.setSuperclass(cls)
+            enhancer.classLoader = cls.classLoader
+            enhancer.setCallback(object : MethodInterceptor {
+                override fun intercept(
+                    obj: Any?,
+                    method: Method,
+                    args: Array<out Any>?,
+                    proxy: MethodProxy
+                ): Any? {
+                    if (method.name == "hashCode"
+                        || method.name == "equals"
+                        || method.name == "toString"
+                    ) {
+                        return proxy.invokeSuper(obj, args)
+                    }
+                    throw NotImplementedError()
+                }
+            })
+            return enhancer.create()
+        } catch (e: Exception) {
+            throw CreateUndefinedException("Failed to create instance with cglib", e)
+        }
+    }
+
+    @PublishedApi
+    internal fun createUnsafeInstance(cls: Class<*>): Any {
+        try {
+            val field = Unsafe::class.java.getDeclaredField("theUnsafe")
+            field.isAccessible = true
+            return (field.get(null) as Unsafe).allocateInstance(cls)
+        } catch (e: Exception) {
+            throw CreateUndefinedException("Failed to create instance with Unsafe", e)
+        }
     }
 
     public inline fun <reified T> getValueOrUndefined(entity: Entity<*>, columnBinding: ColumnBinding): T {
