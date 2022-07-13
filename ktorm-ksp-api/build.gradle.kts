cf1e27c1d85a72588acdd8ce8af46eb7ea5d8a53
夜里的向日葵 <641571835@qq.com>
2022-07-13 11:21:19 +0800

    add multi jdk tests

--- ktorm-ksp-api/build.gradle.kts
+++ ktorm-ksp-api/build.gradle.kts
@@ -11,3 +11,27 @@ dependencies {
 }
 
 configureMavenPublishing()
+
+for (majorVersion in 8..17) {
+    // Adoptium JDK 9 cannot extract on Linux or Mac OS.
+    if (majorVersion == 9) continue
+
+    val jdkTest = tasks.register<Test>("testJdk$majorVersion") {
+        val javaToolchains = project.extensions.getByType(JavaToolchainService::class)
+        javaLauncher.set(javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(majorVersion))
+        })
+
+        description = "Runs the test suite on JDK $majorVersion"
+        group = LifecycleBasePlugin.VERIFICATION_GROUP
+
+        // Copy inputs from normal Test task.
+        val testTask = tasks.getByName<Test>("test")
+        classpath = testTask.classpath
+        testClassesDirs = testTask.testClassesDirs
+
+    }
+    tasks.named<Test>("test").configure {
+        dependsOn(jdkTest)
+    }
+}
