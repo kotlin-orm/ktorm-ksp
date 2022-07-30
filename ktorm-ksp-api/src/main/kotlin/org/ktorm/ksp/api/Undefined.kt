86467c1dd613528a0e42fba819716913fd2e294c
vince <me@liuwj.me>
2022-07-30 21:54:20 +0800

    define class by reflection

--- ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/Undefined.kt
+++ ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/Undefined.kt
@@ -17,13 +17,16 @@
 package org.ktorm.ksp.api
 
 import sun.misc.Unsafe
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import java.lang.reflect.Proxy
 import java.nio.ByteBuffer
+import java.security.ProtectionDomain
 import java.util.concurrent.ConcurrentHashMap
 
-private val unsafe = getUnsafe()
 private val undefinedValuesCache = ConcurrentHashMap<Class<*>, Any>()
+private val unsafe = getUnsafe()
+private val defineClassMethod = getDefineClassMethod()
 
 public inline fun <reified T> undefined(): T {
     return getUndefinedValue(T::class.java) as T
@@ -69,53 +72,48 @@ private fun createUndefinedValueByJdkProxy(cls: Class<*>): Any {
 }
 
 private fun createUndefinedValueBySubclassing(cls: Class<*>): Any {
-    var subclassName = cls.name + "\$KtormUndefined"
-    if (cls.name.startsWith("java.")) {
+    val superClassName = cls.name.replace(".", "/")
+
+    var subclassName = "$superClassName\$KtormUndefined"
+    if (superClassName.startsWith("java/")) {
         subclassName = "\$" + subclassName
     }
 
-    val classLoader = SubclassingClassLoader(cls.classLoader)
-    val subclass = Class.forName(subclassName, true, classLoader)
+    val bytes = generateByteCode(subclassName.toByteArray(), superClassName.toByteArray())
+    val subclass = defineClassMethod.invoke(cls.classLoader, null, bytes, null) as Class<*>
     return unsafe.allocateInstance(subclass)
 }
 
-private class SubclassingClassLoader(parent: ClassLoader) : ClassLoader(parent) {
+private fun getDefineClassMethod(): Method {
+    val parameterTypes = arrayOf(String::class.java, ByteBuffer::class.java, ProtectionDomain::class.java)
+    val method = ClassLoader::class.java.getDeclaredMethod("defineClass", *parameterTypes)
+    method.isAccessible = true
+    return method
+}
 
-    override fun findClass(name: String): Class<*> {
-        if (!name.endsWith("\$KtormUndefined")) {
-            throw ClassNotFoundException(name)
-        } else {
-            val subclassName = name.replace(".", "/")
-            val superClassName = subclassName.removePrefix("\$").removeSuffix("\$KtormUndefined")
-            val bytes = generateByteCode(subclassName.toByteArray(), superClassName.toByteArray())
-            return defineClass(name, bytes, null)
-        }
-    }
-
-    private fun generateByteCode(className: ByteArray, superClassName: ByteArray): ByteBuffer {
-        val buf = ByteBuffer.allocate(1024)
-        buf.putInt(0xCAFEBABE.toInt())                          // magic
-        buf.putShort(0)                                         // minor version
-        buf.putShort(52)                                        // major version, 52 for JDK1.8
-        buf.putShort(5)                                         // constant pool count, totally 4 constants, so it's 5
-        buf.put(1)                                              // #1, CONSTANT_Utf8_info
-        buf.putShort(className.size.toShort())                  // length
-        buf.put(className)                                      // class name
-        buf.put(7)                                              // #2, CONSTANT_Class_info
-        buf.putShort(1)                                         // name index, ref to constant #1
-        buf.put(1)                                              // #3, CONSTANT_Utf8_info
-        buf.putShort(superClassName.size.toShort())             // length
-        buf.put(superClassName)                                 // super class name
-        buf.put(7)                                              // #4, CONSTANT_Class_info
-        buf.putShort(3)                                         // name index, ref to constant #3
-        buf.putShort((0x0001 or 0x0020 or 0x1000).toShort())    // access flags, ACC_PUBLIC | ACC_SUPER | ACC_SYNTHETIC
-        buf.putShort(2)                                         // this class, ref to constant #2
-        buf.putShort(4)                                         // super class, ref to constant #4
-        buf.putShort(0)                                         // interfaces count
-        buf.putShort(0)                                         // fields count
-        buf.putShort(0)                                         // methods count
-        buf.putShort(0)                                         // attributes count
-        buf.flip()
-        return buf
-    }
+private fun generateByteCode(className: ByteArray, superClassName: ByteArray): ByteBuffer {
+    val buf = ByteBuffer.allocate(1024)
+    buf.putInt(0xCAFEBABE.toInt())                          // magic
+    buf.putShort(0)                                         // minor version
+    buf.putShort(52)                                        // major version, 52 for JDK1.8
+    buf.putShort(5)                                         // constant pool count, totally 4 constants, so it's 5
+    buf.put(1)                                              // #1, CONSTANT_Utf8_info
+    buf.putShort(className.size.toShort())                  // length
+    buf.put(className)                                      // class name
+    buf.put(7)                                              // #2, CONSTANT_Class_info
+    buf.putShort(1)                                         // name index, ref to constant #1
+    buf.put(1)                                              // #3, CONSTANT_Utf8_info
+    buf.putShort(superClassName.size.toShort())             // length
+    buf.put(superClassName)                                 // super class name
+    buf.put(7)                                              // #4, CONSTANT_Class_info
+    buf.putShort(3)                                         // name index, ref to constant #3
+    buf.putShort((0x0001 or 0x0020 or 0x1000).toShort())    // access flags, ACC_PUBLIC | ACC_SUPER | ACC_SYNTHETIC
+    buf.putShort(2)                                         // this class, ref to constant #2
+    buf.putShort(4)                                         // super class, ref to constant #4
+    buf.putShort(0)                                         // interfaces count
+    buf.putShort(0)                                         // fields count
+    buf.putShort(0)                                         // methods count
+    buf.putShort(0)                                         // attributes count
+    buf.flip()
+    return buf
 }
