cdc40fa98d368acf3fa45b691d6f8ae993171952
vince <me@liuwj.me>
2023-02-10 20:40:27 +0800

    fix snake case convert

--- ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/NamingStrategy.kt
+++ ktorm-ksp-api/src/main/kotlin/org/ktorm/ksp/api/NamingStrategy.kt
@@ -53,26 +53,14 @@ public interface NamingStrategy {
 public object CamelCaseToSnakeCaseNamingStrategy : NamingStrategy {
 
     override fun toTableName(entityClassName: String): String {
-        return entityClassName.camelCase().lowercase()
+        return entityClassName.camelCase()
     }
 
     override fun toColumnName(propertyName: String): String {
-        return propertyName.camelCase().lowercase()
+        return propertyName.camelCase()
     }
 
     private fun String.camelCase(): String {
-        val builder = StringBuilder(this)
-        var i = 1
-        while (i < builder.length - 1) {
-            if (isUnderscoreRequired(builder[i - 1], builder[i], builder[i + 1])) {
-                builder.insert(i++, '_')
-            }
-            i++
-        }
-        return builder.toString()
-    }
-
-    private fun isUnderscoreRequired(before: Char, current: Char, after: Char): Boolean {
-        return Character.isLowerCase(before) && Character.isUpperCase(current) && Character.isLowerCase(after)
+        return replace(Regex("([a-z])([A-Z])"), "$1_$2").replace(Regex("([A-Z])([A-Z][a-z])"), "$1_$2").lowercase()
     }
 }
