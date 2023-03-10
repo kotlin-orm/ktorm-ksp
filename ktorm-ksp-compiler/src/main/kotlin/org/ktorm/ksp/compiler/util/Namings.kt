a9eec9e33f165cff6e0ee095a9fc0c75a47e3ec9
vince <me@liuwj.me>
2023-03-10 21:33:01 +0800

    udpate comment

--- ktorm-ksp-compiler/src/main/kotlin/org/ktorm/ksp/compiler/util/Namings.kt
+++ ktorm-ksp-compiler/src/main/kotlin/org/ktorm/ksp/compiler/util/Namings.kt
@@ -62,7 +62,7 @@ object DefaultCodingNamingStrategy : CodingNamingStrategy {
 }
 
 private object CamelCase {
-    // Matches boundary among words, for example (abc|Def), (ABC|Def)
+    // Matches boundaries among words, for example (abc|Def), (ABC|Def)
     private val boundaries = listOf(Regex("([a-z])([A-Z])"), Regex("([A-Z])([A-Z][a-z])"))
 
     fun toLowerSnakeCase(name: String): String {
