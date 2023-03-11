5c4e240642d65b42bbbfa1477d7bdab96074961b
vince <me@liuwj.me>
2023-03-11 22:50:46 +0800

    refactor test

--- ktorm-ksp-compiler/src/test/kotlin/org/ktorm/ksp/compiler/test/KtormKspTest.kt
+++ ktorm-ksp-compiler/src/test/kotlin/org/ktorm/ksp/compiler/test/KtormKspTest.kt
@@ -21,28 +21,26 @@ import org.junit.Test
 class KtormKspTest : BaseTest() {
 
     @Test
-    fun `multi primary key`() {
-        runKotlin("""
-            @Table(name = "province")
-            data class Province(
-                @PrimaryKey
-                val country:String,
-                @PrimaryKey
-                val province:String,
-                var population:Int
-            )
+    fun `multi primary key`() = runKotlin("""
+        @Table(name = "province")
+        data class Province(
+            @PrimaryKey
+            val country:String,
+            @PrimaryKey
+            val province:String,
+            var population:Int
+        )
+        
+        fun run() {
+            database.provinces.add(Province("China", "Guangdong", 150000))
+            assert(database.provinces.toList().contains(Province("China", "Guangdong", 150000)))
             
-            fun run() {
-                database.provinces.add(Province("China", "Guangdong", 150000))
-                assert(database.provinces.toList().contains(Province("China", "Guangdong", 150000)))
-                
-                var province = database.provinces.first { (it.country eq "China") and (it.province eq "Hebei") }
-                province.population = 200000
-                database.provinces.update(province)
-                
-                province = database.provinces.first { (it.country eq "China") and (it.province eq "Hebei") }
-                assert(province.population == 200000)
-            }
-        """.trimIndent())
-    }
+            var province = database.provinces.first { (it.country eq "China") and (it.province eq "Hebei") }
+            province.population = 200000
+            database.provinces.update(province)
+            
+            province = database.provinces.first { (it.country eq "China") and (it.province eq "Hebei") }
+            assert(province.population == 200000)
+        }
+    """.trimIndent())
 }
