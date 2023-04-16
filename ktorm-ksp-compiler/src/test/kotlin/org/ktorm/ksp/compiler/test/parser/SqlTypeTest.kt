2388447c30a96ec95d67feb957ecc2300d678287
vince <me@liuwj.me>
2023-04-16 09:59:48 +0800

    refactor test

--- ktorm-ksp-compiler/src/test/kotlin/org/ktorm/ksp/compiler/test/parser/SqlTypeTest.kt
+++ ktorm-ksp-compiler/src/test/kotlin/org/ktorm/ksp/compiler/test/parser/SqlTypeTest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.ktorm.ksp.compiler.test.config
+package org.ktorm.ksp.compiler.test.parser
 
 import org.junit.Test
 import org.ktorm.ksp.compiler.test.BaseTest
